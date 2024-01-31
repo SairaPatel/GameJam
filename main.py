f491da142b7df981b49b1fcb8f1d343bda3309e8
Saira Patel <sairapatel1@outlook.com>
2024-01-31 00:33:48 +0000

    updated rocket class and main code to allow rocket to move left and right on keyboard input. Also created a Star class which generate and update its random position and edited the main code to generate 50 Stars.

--- main.py
+++ main.py
@@ -3,15 +3,11 @@ import math
 import pygame, sys
 from pygame.locals import *
 
-from classes import *
+from rocket import *
+
+
 
 pygame.init()
-
-# SET WINDOW
-WIDTH = 400
-HEIGHT = 500
-win = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Space Game")
 
 
 # COLOURS
@@ -19,6 +15,14 @@ white = pygame.Color(255, 255, 255)
 blue = pygame.Color(0, 0, 40)
 black = pygame.Color(0, 0, 0)
 red = pygame.Color(255, 0, 0)
+
+
+# SET WINDOW
+WIDTH = 400
+HEIGHT = math.floor(WIDTH*1.5)
+
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space Game")
 
 
 # SET CLOCK
@@ -29,37 +33,46 @@ fps = 30
 
 # SPRITES
 
-# rocket
-rocket = Rocket(red, math.floor(WIDTH/10), math.floor(WIDTH/2), math.floor(HEIGHT/2))
-
-
 # overall sprite group
 sprites = pygame.sprite.Group()
+stars = pygame.sprite.Group()
+
+# stars
+for i in range(50):
+    star = Star(white, WIDTH, HEIGHT)
+    stars.add(star)
+
+sprites.add(stars)# rocket
+rocket = Rocket(red, math.floor(WIDTH/10), math.floor(WIDTH/2), math.floor(HEIGHT/2))
 sprites.add(rocket)
 
+
+    
 # MAIN GAME LOOP
 run = True
 while run:
-     # clock
-     clock.tick(fps)
+    # clock
+    clock.tick(fps)
 
      
-     # EVENTS
-     for event in pygame.event.get():
-          # QUIT GAME
-          if event.type == QUIT:
-               pygame.quit()
-               sys.exit()
-
-          
-
-     # update display
-     win.fill(blue)
-     sprites.draw(win)     
-     pygame.display.update()
+    # EVENTS
+    for event in pygame.event.get():
+        # QUIT GAME
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
 
 
-class game():
-     def __init__(self, windowWidth):
-          self.windowWidth = windowWidth
-          
+    # KEYS PRESSED
+    keys = pygame.key.get_pressed();
+    rocket.updatePos(keys[pygame.K_LEFT], keys[pygame.K_RIGHT])
+    
+    # update stars - CHANGE THIS: create a Stars class, to hold a list of stars and automatically update all their pos with one method call
+    for s in stars.sprites():
+        s.updatePos(rocket.speed)
+
+    # update display
+    win.fill(blue)
+    sprites.draw(win)     
+    pygame.display.update()
+
--- rocket.py
+++ rocket.py
@@ -1,4 +1,5 @@
 import math
+import random
 
 import pygame, sys
 from pygame.locals import *
@@ -6,13 +7,70 @@ from pygame.locals import *
 
 # ROCKET CLASS
 class Rocket(pygame.sprite.Sprite):
-     def __init__(self, colour, width, centre_x, y):
+    def __init__(self, colour, width, centre_x, y):
           
-          super().__init__()
+        super().__init__()
 
-          # set surface
-          self.image = pygame.Surface([width, width*2])
-          self.image.fill(colour)
-          self.rect = self.image.get_rect()
-          self.rect.x = centre_x - math.floor(width/2)
-          self.rect.y = y
+        # set surface
+        self.image = pygame.Surface([width, width*2])
+        self.image.fill(colour)
+        self.rect = self.image.get_rect()
+        self.rect.x = centre_x - math.floor(width/2)
+        self.rect.y = y
+
+
+        # set game vars
+        self.speed = 5
+        self.score = 0
+        
+
+    def updatePos(self, left_key, right_key):
+        
+        if left_key:
+            self.rect.x -= self.speed
+            
+            
+        if right_key:
+            self.rect.x += self.speed
+
+    
+class Star(pygame.sprite.Sprite):
+    def __init__(self, colour, win_width, win_height):
+          
+        super().__init__()
+        self.win_width = win_width
+        self.win_height = win_height
+
+        self.width = random.randrange(3,8,2)
+        self.height = self.width
+
+        # set surface
+        self.image = pygame.Surface([self.width, self.height])
+        self.rect = self.image.get_rect()
+
+        # set background transparent
+        self.image.fill(pygame.Color(0,0,0))
+        self.image.set_colorkey(pygame.Color(0,0,0))
+
+        # draw star lines
+        if random.randint(0, 3) >= 1:
+            pygame.draw.line(self.image, colour, (self.rect.centerx, 0), (self.rect.centerx, self.height))
+            pygame.draw.line(self.image, colour, (0, self.rect.centery), (self.width,self.rect.centery))
+        else:
+            pygame.draw.circle(self.image, colour, self.rect.center, random.randint(1, self.width))
+          
+        self.setPos()
+
+
+    def setPos(self):
+        self.rect.x = random.randint(0, self.win_width)
+        self.rect.y = random.randint(0, self.win_height)
+
+    def updatePos(self, speed):
+        if self.rect.y < self.win_height + self.height:
+            self.rect.y += speed
+        else:
+            self.rect.x = random.randint(0, self.win_width)
+            self.rect.y = -1 * random.randint(self.height, self.height*5)
+
+   
